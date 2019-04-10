aad23dbf0fa82d40f21c1f9c38b12fb10f6a103e
leejinseok <leejinseok@wemakeprice.com>
2019-04-10 18:15:02 +0900

    Admin page default set

--- index.js
+++ index.js
@@ -2,33 +2,37 @@
 
 const os = require('os');
 const cluster = require('cluster');
+const config = require('config');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const app = express();
-const config = require('config');
-const api = require('./routes/api');
-const admin = require('./routes/admin');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
 const accessCrossDomain = require('./middleware/access-cross-domain');
 const apiHeaderParser = require('./middleware/api-header-parser');
 const errorHandler = require('./middleware/error-handler');
+const api = require('./routes/api');
+const admin = require('./routes/admin');
 const models = require('./models');
+const app = express();
 
 app.disable('x-powered-by');
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('tiny'));
+app.use(compression());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: false, limit: '1mb'}));
 app.use(bodyParser.json());
 app.use(accessCrossDomain);
 app.use(apiHeaderParser);
+app.use('/public', express.static(`${__dirname}/public`));
 app.use('/admin', admin);
 app.use('/api', api);
 app.use(errorHandler);
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).send('404 Not Found!');
 });
 
